61a73b33bf07a6538283fd69a07825ed397715d8
Ivan Birkman <ivan.yk14@gmail.com>
2018-03-06 12:52:22 +0100

    Add size selector to assessments table in mine site

--- components/pages/mine-sites-detail/mine-sites-detail-assessments-table/mine-sites-detail-assessments-table-constants.js
+++ components/pages/mine-sites-detail/mine-sites-detail-assessments-table/mine-sites-detail-assessments-table-constants.js
@@ -31,4 +31,23 @@ export const ASSESSMENTS_TABLE_COLUMNS = [
   }
 ];
 
-export default { ASSESSMENTS_TABLE_COLUMNS };
+export const TABLE_SIZE_VALUES = [
+  {
+    label: '5',
+    value: 5
+  },
+  {
+    label: '10',
+    value: 10
+  },
+  {
+    label: '15',
+    value: 15
+  },
+  {
+    label: '20',
+    value: 20
+  },
+];
+
+export default { ASSESSMENTS_TABLE_COLUMNS, TABLE_SIZE_VALUES };
