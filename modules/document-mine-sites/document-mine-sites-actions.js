acc095fe86578567ffb15401b3f7ae563ef9676a
Ivan Birkman <ivan.yk14@gmail.com>
2018-03-12 18:20:41 +0100

    Remove comment

--- modules/document-mine-sites/document-mine-sites-actions.js
+++ modules/document-mine-sites/document-mine-sites-actions.js
@@ -50,18 +50,3 @@ export default {
   setDocumentMineSites,
   getDocumentMineSites
 };
-
-
-
-
-// export const getDocumentMineSites = createThunkAction('document-mine-sites/getDocumentMineSites', _options =>
-//   dispatch =>
-//     new Promise((resolve, reject) => {
-//       documentMineSitesService.getDocumentMineSites(_options)
-//         .then((data) => {
-//           const parsedData = new Jsona().deserialize(data);
-//           resolve(parsedData);
-//           dispatch(setDocumentMineSites(parsedData));
-//         })
-//         .catch(errors => reject(errors));
-//     }));
