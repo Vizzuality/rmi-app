8c35b6833389635d0cd6a8504527f38b19a94176
Ivan Birkman <ivan.yk14@gmail.com>
2018-03-20 15:16:04 +0100

    Add label to mine site scores bars chart

--- components/pages/mine-sites-detail/mine-sites-detail-bars/mine-sites-detail-bars-constants.js
+++ components/pages/mine-sites-detail/mine-sites-detail-bars/mine-sites-detail-bars-constants.js
@@ -3,7 +3,10 @@ export const CHART_CONFIG = {
   barSize: 85,
   setBarFill: () => '#bf3132',
   domain: [0, 6],
-  YAxisTicks: ['0.0', '1.0', '2.0', '3.0', '4.0', '5.0', '6.0']
+  YAxisTicks: ['0.0', '1.0', '2.0', '3.0', '4.0', '5.0', '6.0'],
+  xAxisKey: 'label',
+  xAxisTick: true,
+  xAxisHeight: 25
 };
 
 export default { CHART_CONFIG };
