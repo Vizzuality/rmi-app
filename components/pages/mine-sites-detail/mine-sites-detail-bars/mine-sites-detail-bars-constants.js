3b0413825680f325fffee0b7ee29370c6c6b141e
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2018-04-02 09:24:25 +0200

    Displays all labels on x-axis

--- components/pages/mine-sites-detail/mine-sites-detail-bars/mine-sites-detail-bars-constants.js
+++ components/pages/mine-sites-detail/mine-sites-detail-bars/mine-sites-detail-bars-constants.js
@@ -3,10 +3,15 @@ export const CHART_CONFIG = {
   barSize: 85,
   setBarFill: () => '#bf3132',
   domain: [0, 6],
-  YAxisTicks: ['0.0', '1.0', '2.0', '3.0', '4.0', '5.0', '6.0'],
+  // xAxis
   xAxisKey: 'label',
   xAxisTick: true,
-  xAxisHeight: 25
+  xAxisHeight: 125,
+  xAxisTick: { angle: -45, width: 125 },
+  xAxisInterval: 0,
+  xAxisTextAnchor: 'end',
+  // y Axis
+  YAxisTicks: ['0.0', '1.0', '2.0', '3.0', '4.0', '5.0', '6.0'],
 };
 
 export default { CHART_CONFIG };
