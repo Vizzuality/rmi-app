28f7d327ff05e5f52c513a38d53f902c2da14eb8
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2018-04-25 12:39:55 +0200

    Parses on mobile

--- components/layout/language-bar-mobile/index.js
+++ components/layout/language-bar-mobile/index.js
@@ -3,12 +3,15 @@ import { connect } from 'react-redux';
 // actions
 import { setCurrentLanguage } from 'modules/language/languages-actions';
 
+// selectors
+import { parseLanguages } from './language-bar-mobile-selectors';
+
 // component
 import LanguageBarMobile from './language-bar-mobile-component';
 
 export default connect(
   state => ({
-    languages: state.language.list,
+    languages: parseLanguages(state),
     currentLanguage: state.language.current
   }),
   { setCurrentLanguage }
