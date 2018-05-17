0e5eddeb6b470082bd05f45c83252a953c9cb794
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2018-05-17 13:58:36 +0200

    Updates pagination values for library

--- components/pages/library/library-table/library-table-constants.js
+++ components/pages/library/library-table/library-table-constants.js
@@ -27,20 +27,20 @@ export const DOCUMENTS_TABLE_COLUMNS = [
 
 export const TABLE_SIZE_VALUES = [
   {
-    label: '5',
-    value: 5
-  },
-  {
     label: '10',
     value: 10
   },
   {
-    label: '15',
-    value: 15
-  },
-  {
     label: '20',
     value: 20
+  },
+  {
+    label: '50',
+    value: 50
+  },
+  {
+    label: '100',
+    value: 100
   }
 ];
 
